378ad5627275d795063c81c5b460f4d2b3672323
gamonges <0627gamonges@gmail.com>
2020-04-29 21:19:31 +0900

    https対応

--- alb/main.tf
+++ alb/main.tf
@@ -117,6 +117,79 @@ resource "aws_lb_listener" "http" {
     }
 }
 
+resource "aws_acm_certificate" "example" {
+    domain_name = aws_route53_record.example.domain_name
+    subjrct_alternative_names = []
+    validation_method = "DNS"
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_acm_certificate_validation" "exmaple" {
+    certificate_arn = aws_acm_certificate.example.arn
+    validation_record_fqdns = [aws_route53_record.exmaple_certificate.fqdn]
+}
+
+resource "aws_lb_listener" "https" {
+    load_balancer_arn = aws_lb.terraform-test-alb.arn
+    port = "443"
+    protocol = "HTTPS"
+    certificate_arn = aws_acm_certificate.example.arn
+    ssl_policy = "ELBSecurityPolicy-2016-08"
+
+    default_action {
+        type = "fixed-response"
+    }
+
+    fixed_response {
+        content_type = "text/plain"
+        message_body = "これは「HTTPS」です。"
+        status_code = "200"
+    }
+}
+
+resource "aws_lb_listener" "redirect_http_to_https" {
+    load_balancer_arn = aws_lb.terraform-test-alb.arn
+    port = "8080"
+    protocol = "HTTP"
+
+    default_action {
+        type = "redirect"
+    }
+
+    redirect {
+        port = "443"
+        protocol = "HTTPS"
+        status_code = "HTTP_301"
+    }
+}
+
+resource "aws_lb_target_group" "example" {
+    name = "example"
+    target_type = "ip"
+    vpc_id = aws_vpc.example.id
+    port = 80
+    protocol = "HTTP"
+    deregistration_delay = 300
+
+    health_check {
+        path = "/"
+        healthy_threshhold = 5
+        unhealthy_threshholod = 2
+        timout = 5
+        interval = 30
+        matcher = 200
+        port = "traffic-port"
+        protocol = "HTTP"
+    }
+
+    depends_on = [aws_lb.example]
+}
+
+
+
 output "alb_dns_name" {
     value = aws_lb.terraform-test-alb.dns_name
 }
